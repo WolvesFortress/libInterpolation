4dbcd1c85123e79c54af66cda6491e6d13cb813c
radondev <mail.radondev@gmail.com>
2021-08-05 20:26:41 +0200

    New quadratic interpolation functions;

--- src/radondev/libInterpolation/Interpolation.php
+++ src/radondev/libInterpolation/Interpolation.php
@@ -37,7 +37,90 @@ class Interpolation
     }
 
     /**
+     * @see https://en.wikipedia.org/wiki/B%C3%A9zier_curve#Quadratic_B%C3%A9zier_curves
+     *
+     * @param float $first
+     * @param float $second
+     * @param float $third
+     * @param float $percentage
+     * @return float
+     */
+    public static function quadraticBezierValue(float $first, float $second, float $third, float $percentage): float
+    {
+        $inversePercentage = (1 - $percentage);
+
+        return ($inversePercentage ** 2 * $first) + (2 * $inversePercentage * $percentage * $second) + ($percentage ** 2 * $third);
+    }
+
+    /**
+     * @see https://en.wikipedia.org/wiki/B%C3%A9zier_curve#Quadratic_B%C3%A9zier_curves
+     *
+     * @param Vector3 $first
+     * @param Vector3 $second
+     * @param Vector3 $third
+     * @param float $percentage
+     * @return Vector3
+     */
+    public static function quadraticBezierVector(Vector3 $first, Vector3 $second, Vector3 $third, float $percentage): Vector3
+    {
+        $inversePercentage = (1 - $percentage);
+
+        $poly1 = $first->multiply($inversePercentage ** 2);
+        $poly2 = $second->multiply(2 * $inversePercentage * $percentage);
+        $poly3 = $third->multiply($percentage ** 2);
+
+        return $poly1->addVector($poly2)->addVector($poly3);
+    }
+
+    /**
+     * @see https://en.wikipedia.org/wiki/De_Casteljau%27s_algorithm
+     * @see https://en.wikipedia.org/wiki/B%C3%A9zier_curve#Quadratic_B%C3%A9zier_curves
+     *
+     * @param float $first
+     * @param float $second
+     * @param float $third
+     * @param float $percentage Number between 0.0 and 1.0
+     * @return float
+     */
+    public static function quadraticDeCasteljauValue(float $first, float $second, float $third, float $percentage): float
+    {
+        $inversePercentage = (1 - $percentage);
+
+        $d1 = ($inversePercentage * $first) + ($percentage * $second);
+        $d2 = ($inversePercentage * $second) + ($percentage * $third);
+
+        return ($inversePercentage * $d1) + ($percentage * $d2);
+    }
+
+    /**
+     * @see https://en.wikipedia.org/wiki/De_Casteljau%27s_algorithm
+     * @see https://en.wikipedia.org/wiki/B%C3%A9zier_curve#Quadratic_B%C3%A9zier_curves
+     *
+     * @param Vector3 $first
+     * @param Vector3 $second
+     * @param Vector3 $third
+     * @param float $percentage Number between 0.0 and 1.0
+     * @return Vector3
+     */
+    public static function quadraticDeCasteljauVector(Vector3 $first, Vector3 $second, Vector3 $third, float $percentage): Vector3
+    {
+        $inversePercentage = (1 - $percentage);
+
+        $d1 = $first->multiply($inversePercentage)->addVector(
+            $second->multiply($percentage)
+        );
+        $d2 = $second->multiply($inversePercentage)->addVector(
+            $third->multiply($percentage)
+        );
+
+        return $d1->multiply($inversePercentage)->addVector(
+            $d2->multiply($percentage)
+        );
+    }
+
+    /**
      * Cubic interpolation based on two values and the given percentage/progress.
+     * Smooths the movement between the two values by accelerating and decelerating.
      * Source: "Math for Game Developers - Removing Abrupt Transitions (Cubic Interpolation)" by Jorge Rodriguez (https://youtu.be/PqQH3r5Ia-Y)
      * @see https://youtu.be/PqQH3r5Ia-Y
      *
@@ -46,7 +129,7 @@ class Interpolation
      * @param float $percentage
      * @return float
      */
-    public static function cubic(float $first, float $second, float $percentage): float
+    public static function cubicSmooth(float $first, float $second, float $percentage): float
     {
         return ($second - $first) * (-2 * $percentage ** 3 + 3 * $percentage ** 2) + $first;
     }
@@ -84,13 +167,13 @@ class Interpolation
      */
     public static function catmullRomSplines(Vector3 $p0, Vector3 $p1, Vector3 $p2, Vector3 $p3, float $percentage, float $alpha = 0.5): Vector3
     {
-        $tSquared = $percentage ** 2;
-        $tCubed = $percentage ** 3;
+        $percentageSquared = $percentage ** 2;
+        $percentageCubed = $percentage ** 3;
 
-        $q1 = -$tCubed + 2.0 * $tSquared - $percentage;
-        $q2 = 3.0 * $tCubed - 5.0 * $tSquared + 2.0;
-        $q3 = -3.0 * $tCubed + 4.0 * $tSquared + $percentage;
-        $q4 = $tCubed - $tSquared;
+        $q1 = -$percentageCubed + 2.0 * $percentageSquared - $percentage;
+        $q2 = 3.0 * $percentageCubed - 5.0 * $percentageSquared + 2.0;
+        $q3 = -3.0 * $percentageCubed + 4.0 * $percentageSquared + $percentage;
+        $q4 = $percentageCubed - $percentageSquared;
 
         return new Vector3(
             $alpha * ($p0->getX() * $q1 + $p1->getX() * $q2 + $p2->getX() * $q3 + $p3->getX() * $q4),
